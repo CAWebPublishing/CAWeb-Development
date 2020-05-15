47e6aea26ca2cd8bb8860bf2728f258c4092e38f
Danny Guzman <guzmanjd86@gmail.com>
2020-05-15 09:49:25 -0700

    Added Query Monitor plugin to list of plugins Updated Default Post/Pages Removed sample scripts

---
+++ wordpress/custom.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Add any additional commands below
+####################################
--- wordpress/samples/scripts/widgets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wp widget delete search-2 --path=$WEB_ROOT --allow-root
-wp widget delete recent-posts-2 --path=$WEB_ROOT --allow-root
-wp widget delete recent-comments-2 --path=$WEB_ROOT --allow-root
